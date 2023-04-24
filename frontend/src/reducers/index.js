91dfd7e5181903ab3c05ce51f78b1acbdfc97655
unknown <Vanessa@W10Vanessa.ibisg.it>
2023-04-24 12:34:08 +0200

    introduction redux

---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,13 @@
+import initialState from "../store/index"
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+
+        case "HANDLE_INPUT_CHANGE":
+            break;
+
+        case "ADD_NEW_MEMBER":
+            break;
+    }
+
+    return state;
+}
---
+++ frontend/src/store/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+    users: {
+        login: false,
+        account:{
+            codiceMembro:"",
+            password:"",
+            email:""
+        },
+        ui: {
+            //All the UI related state here. eg: hide/show modals,
+            //toggle checkbox etc.
+        }
+    }
+}
